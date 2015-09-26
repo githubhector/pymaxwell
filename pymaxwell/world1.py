a51af1f428c09981a67545f205fa3cb206c61fb7
Hector Urroz <hurroz@gmail.com>
2015-09-25 20:37:00 -0600

    Start fleshing out World1

--- pymaxwell/world1.py
+++ pymaxwell/world1.py
@@ -1,12 +1,22 @@
 class World1:
 
     world_name = "World1"
+    NUM_PARTICLES = 5
 
     def __init__(self):
         pass
 
     def init(self):
         print "Initializing instance of World1..."
+        print "Creating particles list..."
+        self.particles = list()
+        for i in range(0, self.NUM_PARTICLES):
+            print i
+            self.particles.append(Particle())
 
     def update(self):
         pass
+
+class Particle(object):
+    pass
+
