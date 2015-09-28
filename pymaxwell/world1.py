ec0789fa13a35110f37ad5e357e15167ca878b94
Hector Urroz <hurroz@gmail.com>
2015-09-28 16:02:05 -0600

    Create classes for bid, particle, and point

--- pymaxwell/world1.py
+++ pymaxwell/world1.py
@@ -14,7 +14,22 @@ class World1:
     def update(self):
         pass
 
+class Box():
+    """ A box with given origin, and given x and y lengths. The origin is the
+    lower left corner
+    """
+    def __init__(self, orig, len_x, len_y):
+        self.orig = orig
+        self.len_x = len_x
+        self.len_y = len_y
 
-class Particle(object):
-    pass
+class Particle():
+    def __init__(self):
+        pass
 
+class Point():
+    """ A point at a given coordinate
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
