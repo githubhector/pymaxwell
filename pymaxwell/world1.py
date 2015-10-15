3535428722acf00e256a5e67317dc64c3714c413
Hector Urroz <hurroz@gmail.com>
2015-10-15 12:58:25 -0600

    Convert vector components to floats

--- pymaxwell/world1.py
+++ pymaxwell/world1.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import uniform
 from utils import StringBuilder
 
 
@@ -12,7 +12,7 @@ class World1:
         print "Initializing instance of World1..."
 
         print "Creating box..."
-        self.box = Box((0,0), 100, 50)
+        self.box = Box(Vector(0,0), 100, 50)
 
         print "Adding particles to the box..."
         self.box.add_particles_at_random(self.NUM_PARTICLES)
@@ -29,8 +29,8 @@ class Vector():
     """ A vector with the given x and y components
     """
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
@@ -44,17 +44,17 @@ class Vector():
 
 class Box():
     """ A box with given origin, and x, y lengths. The box's origin is the
-    lower left corner. Expect orig to be a pair (x,y)
+    lower left corner. Expect orig to be a Vector
     """
 
     particles = []
 
     def __init__(self, orig, len_x, len_y):
         self.orig = orig
-        self.len_x = len_x
-        self.len_y = len_y
-        self.x_interval = (orig[0], orig[0] + len_x)
-        self.y_interval = (orig[1], orig[1] + len_y)
+        self.len_x = float(len_x)
+        self.len_y = float(len_y)
+        self.x_interval = (self.orig.x, self.orig.x + len_x)
+        self.y_interval = (self.orig.y, self.orig.y + len_y)
 
     def add_particle(self, particle):
         self.particles.append(particle)
@@ -63,8 +63,8 @@ class Box():
         print "x_interval", self.x_interval
         print "y_interval", self.y_interval
         for i in range(0, num_particles):
-            x_rand = randint(self.x_interval[0], self.x_interval[1])
-            y_rand = randint(self.y_interval[0], self.y_interval[1])
+            x_rand = uniform(self.x_interval[0], self.x_interval[1])
+            y_rand = uniform(self.y_interval[0], self.y_interval[1])
             self.add_particle(Particle(1, Vector(x_rand, y_rand), Vector(0,0)))
             print "x_rand:", x_rand, " y_rand:", y_rand
 
